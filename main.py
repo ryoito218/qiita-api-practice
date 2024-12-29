6a3761a54315aae0b8819ddecfd3fe786d4f0175
ryoito218 <ryoito218@gmail.com>
2024-12-29 12:00:36 +0900

    Practice 20241229

--- main.py
+++ main.py
@@ -8,10 +8,29 @@ API_KEY = os.getenv("API_KEY")
 
 url = 'https://qiita.com/api/v2/items'
 headers = {'Authorization': f'Bearer {API_KEY}'}
-params = {'page': 1, 'per_page': 10, 'query': 'title:python'}
+params = {'page': 1, 'per_page': 20, 'query': 'title:Udemy'}
 
 response = requests.get(url, headers=headers, params=params)
-data = response.json()
 
-for item in data:
-    print(item['title'])
+if response.status_code == 200:
+    data = response.json()
+
+    # for i in range(len(data)):
+    #     print(i+1, data[i]["title"], data[i]["url"])
+    #     print()
+
+    if data:
+        print(data[12]["title"])
+        print(data[12]["rendered_body"])
+        # print(data[12]["body"])
+        print(data[12]["url"])
+        # fields = data[0].keys()
+        # print("フィールド名")
+        # for field in fields:
+        #     print(field)
+
+else:
+    print("エラー")
+
+# for item in data:
+#     print(item['title'])
